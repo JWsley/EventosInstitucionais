ddc7e5b41f392e005de04e507d84edbff0b0242c
Jonathan Wesley Cijani Bertoni França <jonathanwleycijani@gmail.com>
2023-12-06 11:45:15 -0300

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- calendar.php
+++ calendar.php
@@ -1,10 +1,9 @@
 <?php 
 
   include('./js/coleta.php');
-  
+  $events = "coleta.php"
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -17,29 +16,92 @@
 
 
 <script>
-document.addEventListener('DOMContentLoaded', function() {
-      var calendarEl = document.getElementById('calendar');
+    document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
 
-      var calendar = new FullCalendar.Calendar(calendarEl, {
-        headerToolbar: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'listDay,listWeek'
+      },
+
+      // customize the button names,
+      // otherwise they'd all just say "list"
+      views: {
+        listDay: { buttonText: 'list day' },
+        listWeek: { buttonText: 'list week' }
+      },
+
+      initialView: 'dayGridMonth',
+      initialDate: '2023-01-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      locale:'pt-br',
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2023-01-01'
         },
-        initialDate: '2023-01-12',
-        navLinks: true,
-        selectable: true,
-        selectMirror: true,
-        locale: 'pt-br',
-        events: {
-          url: './js/coleta.php', // Altere o caminho para o arquivo coleta.php
-          method: 'GET'
+        {
+          title: 'Long Event',
+          start: '2023-01-07',
+          end: '2023-01-10'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2023-01-09T16:00:00'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2023-01-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2023-01-11',
+          end: '2023-01-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2023-01-12T10:30:00',
+          end: '2023-01-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2023-01-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2023-01-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2023-01-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2023-01-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2023-01-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2023-01-28'
         }
-      });
-
-      calendar.render();
+      ]
     });
-  </script>
+
+    calendar.render();
+  });
+
+    </script>
 
 <body>
 <header class="cabecalho">
@@ -53,9 +115,6 @@ document.addEventListener('DOMContentLoaded', function() {
   
 
 
-
-
-  
 
   <div id="calendar"></div>
 
@@ -72,7 +131,7 @@ document.addEventListener('DOMContentLoaded', function() {
 </footer>
 
 
-<script src="js/script.js"></script>
+
 <script src="js/core/index.global.min.js"></script>
 <script src="js/index.global.min.js"></script>
 <script src="js/core/locales/pt-br.global.min.js"></script>
--- js/coleta.php
+++ js/coleta.php
@@ -1,18 +1,21 @@
 <?php
-// URL que contém o JSON
 $url = 'https://script.googleusercontent.com/a/macros/ifsuldeminas.edu.br/echo?user_content_key=qCcdmkhRBFjz8xo-H5qlUPTnBCDTeql2Hzhqf0i2G4F9d8mXcnK2oDkzswUms3hMxjTDGe0Ma357QuPkLXsfQoqALUYPGTYYOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKCjEak3mjNoJiXSRvGgDgtmtf450f6mFcuHwklkWyWOSDJZu-DkQFVxrrRhnSdGBKf9WFOmbg3LpT1J6vaJ71QqGDNs2YKChA92zhCJaP5YmvSh_C2I_zquSMSstdfO6boqsuGR6F-yhQ&lib=Ms0CmmrMR_PaYaKGVs8OTaxByP_xFMRvX';
 
-
 $jsonString = file_get_contents($url);
-
-
 $array = json_decode($jsonString, true);
 
+$eventos = array();
 
 foreach ($array['output'] as $evento) {
-  echo $evento['nome'] . "<br>";
-  echo $evento['data'] . "<br>";
-  echo $evento['desc'] . "<br> =========================== <br> start:" . $evento['horario_inicio']. "| End:". $evento['horario_fim']."<br> Unidade Responsavel:".$evento['unidade']."<br> Setor responsavel:".$evento['setor'].'<br> Local:'.$evento['local'].'Email:'.$evento['email'];
-
+  $event = array(
+    'title' => $evento['nome'],
+    'start' => $evento['data'] . 'T' . $evento['horario_inicio'],
+    'end' => $evento['data'] . 'T' . $evento['horario_fim'],
+    'description' => $evento['desc'],
+  );
+  array_push($eventos, $event);
 }
+echo "<h4 class='Runn'>Rodando</h4>";
+
+json_encode($eventos);
 ?>
