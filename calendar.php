312d97ebf31c2a036bd8d27b4d8a74460a26ce4e
Jonathan Wesley Cijani Bertoni França <jonathanwleycijani@gmail.com>
2023-12-06 10:47:01 -0300

    updates updates

--- calendar.php
+++ calendar.php
@@ -1,7 +1,7 @@
 <?php 
 
   include('./js/coleta.php');
-
+  
 
 ?>
 
--- js/coleta.php
+++ js/coleta.php
@@ -1,52 +1,18 @@
 <?php
+// URL que contém o JSON
+$url = 'https://script.googleusercontent.com/a/macros/ifsuldeminas.edu.br/echo?user_content_key=qCcdmkhRBFjz8xo-H5qlUPTnBCDTeql2Hzhqf0i2G4F9d8mXcnK2oDkzswUms3hMxjTDGe0Ma357QuPkLXsfQoqALUYPGTYYOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKCjEak3mjNoJiXSRvGgDgtmtf450f6mFcuHwklkWyWOSDJZu-DkQFVxrrRhnSdGBKf9WFOmbg3LpT1J6vaJ71QqGDNs2YKChA92zhCJaP5YmvSh_C2I_zquSMSstdfO6boqsuGR6F-yhQ&lib=Ms0CmmrMR_PaYaKGVs8OTaxByP_xFMRvX';
 
-function listar() {
-  $url = 'https://sheetdb.io/api/v1/o66xzvvp9v2x9';
-  $username = 'vypt00vc';
-  $password = 'ldofh8p69yaput1cti9v';
 
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-  curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$jsonString = file_get_contents($url);
 
-  $response = curl_exec($ch);
-  if($response === false) {
-    echo 'Erro: ' . curl_error($ch);
-  } else {
-    $data = json_decode($response, true);
-    
-    if ($data !== null) {
-        $list_event = array(); // Inicialize o array fora do loop
 
-        foreach ($data as $evento) {
-            $id = 1;
-            $nome = $evento['NOME DO EVENTO'];
-            $horario_inicio = $evento['HORÁRIO - INICIO'];
-            $horario_fim = $evento['HORÁRIO - FIM'];
-            $desc = $evento['DESCRIÇÃO GERAL DO EVENTO'];
-            $unidade = $evento['UNIDADE RESPONSÁVEL PELO EVENTO'];
-            $num = $evento['NÚMERO DE PARTICIPANTES'];
-            $local = $evento['LOCAL DO EVENTO'];
-            $tipo = $evento['TIPO DO EVENTO'];
-            $email = $evento['Endereço de e-mail'];
+$array = json_decode($jsonString, true);
 
-            $list_even[] = [
-                'id' => $id,
-                'title' => $nome,
-                'start' => $horario_inicio,
-                'end' => $horario_fim,  
-                'description' => "Descrição: " . $desc . "<br> ====================== <br> Unidade Responsável: ". $unidade . "<br> Número de participantes:" . $num . " | Tipo de evento:" . $tipo . "<br> Email:". $email . "<br>  Local:". $local
-            ];
-        }
 
-         echo json_encode($list_even); // Envie o JSON uma vez, fora do loop
-    } else {
-        echo "Erro ao decodificar JSON";
-    }
-  }
+foreach ($array['output'] as $evento) {
+  echo $evento['nome'] . "<br>";
+  echo $evento['data'] . "<br>";
+  echo $evento['desc'] . "<br> =========================== <br> start:" . $evento['horario_inicio']. "| End:". $evento['horario_fim']."<br> Unidade Responsavel:".$evento['unidade']."<br> Setor responsavel:".$evento['setor'].'<br> Local:'.$evento['local'].'Email:'.$evento['email'];
+
 }
-
-listar();
-
 ?>
