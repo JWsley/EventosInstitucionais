0569bf981dc6ecbb7daaeac2ffcedc44b9058ad8
Jonathan Wesley Cijani Bertoni França <jonathanwleycijani@gmail.com>
2023-12-07 11:51:13 -0300

    quebra de linhas

--- calendar.php
+++ calendar.php
@@ -1,35 +1,31 @@
 <?php
 
 
-  function eventSheet(){
-    $url = 'https://script.googleusercontent.com/a/macros/ifsuldeminas.edu.br/echo?user_content_key=qCcdmkhRBFjz8xo-H5qlUPTnBCDTeql2Hzhqf0i2G4F9d8mXcnK2oDkzswUms3hMxjTDGe0Ma357QuPkLXsfQoqALUYPGTYYOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKCjEak3mjNoJiXSRvGgDgtmtf450f6mFcuHwklkWyWOSDJZu-DkQFVxrrRhnSdGBKf9WFOmbg3LpT1J6vaJ71QqGDNs2YKChA92zhCJaP5YmvSh_C2I_zquSMSstdfO6boqsuGR6F-yhQ&lib=Ms0CmmrMR_PaYaKGVs8OTaxByP_xFMRvX';
-    $response = file_get_contents($url);
-    $data = json_decode($response);
-  
-    $eventos = array_map(function($evento) {
-      return array(
-         'title' => $evento->nome,
-         'start' => date('c', strtotime($evento->data)),
-         'end' => date('c', strtotime($evento->data)),
-         'description' => $evento->desc,
-         'local' => $evento->local,
-         'horario_inicio' => $evento->horario_inicio,
-         'horario_fim' => $evento->horario_fim,
-         'unidade' => $evento->unidade,
-         'setor' => $evento->setor,
-         'num' => $evento->num,
-         'tipo' => $evento->tipo,
-         'outros' => $evento->outros,
-         'email' => $evento->email,
-      );
-    }, $data->output);
-  
-    return $eventos_json = json_encode($eventos);
-  
+function eventSheet(){
+  $url = 'https://script.googleusercontent.com/a/macros/ifsuldeminas.edu.br/echo?user_content_key=qCcdmkhRBFjz8xo-H5qlUPTnBCDTeql2Hzhqf0i2G4F9d8mXcnK2oDkzswUms3hMxjTDGe0Ma357QuPkLXsfQoqALUYPGTYYOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKCjEak3mjNoJiXSRvGgDgtmtf450f6mFcuHwklkWyWOSDJZu-DkQFVxrrRhnSdGBKf9WFOmbg3LpT1J6vaJ71QqGDNs2YKChA92zhCJaP5YmvSh_C2I_zquSMSstdfO6boqsuGR6F-yhQ&lib=Ms0CmmrMR_PaYaKGVs8OTaxByP_xFMRvX';
+  $response = file_get_contents($url);
+  $data = json_decode($response);
 
-  };
- 
+  $eventos = array_map(function($evento) {
+    return array(
+       'title' => $evento->nome,
+       'start' => $evento->data,
+       'end' => $evento->data,
+       'description' => $evento->desc,
+       'local' => $evento->local,
+       'horario_inicio' => $evento->horario_inicio,
+       'horario_fim' => $evento->horario_fim,
+       'unidade' => $evento->unidade,
+       'setor' => $evento->setor,
+       'num' => $evento->num,
+       'tipo' => $evento->tipo,
+       'outros' => $evento->outros,
+       'email' => $evento->email,
+    );
+  }, $data->output);
 
+  return json_encode($eventos);
+};
 
 
 ?>
@@ -53,6 +49,12 @@
       var calendarEl = document.getElementById('calendar');
 
       var calendar = new FullCalendar.Calendar(calendarEl, {
+        eventRender: function(info) {
+      var eventText = info.event.title;
+      if (eventText.length > 15) { // Defina o comprimento máximo para quebrar a linha conforme necessário
+        info.el.querySelector('.fc-title').innerHTML = eventText.substring(0, 1) + '<br>' + eventText.substring(10);
+      }
+    },
         themeSystem: 'slate', 
           buttonText: {
             today: 'Hoje',
@@ -82,6 +84,16 @@
         eventClick: function(info) {
           alert('Descrição: ' + info.event.extendedProps.description + info.event.extendedProps.start + info.event.extendedProps.end); // Exibe a descrição ao clicar no evento
         },
+        eventContent: function(arg) {
+      return { html: '<div class="event-title">' + arg.event.title + '</div>' };
+    },
+
+    eventDidMount: function(info) {
+      var eventTitle = info.el.querySelector('.event-title');
+      if (eventTitle.offsetHeight > 15) { // Defina a altura máxima para quebrar a linha conforme necessário
+        eventTitle.style.whiteSpace = 'normal';
+      }
+    },
         events: eventos
       });
 
